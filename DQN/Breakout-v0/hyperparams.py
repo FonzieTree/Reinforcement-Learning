2269518385dbf9466238ad44b58c0ffff37ba3a0
Fonzie <675688853@qq.com>
2019-02-07 19:13:20 +0800

    Create hyperparams.py

---
+++ DQN/Breakout-v0/hyperparams.py
@@ -0,0 +1,10 @@
+class Hyperparams:
+    batch_size = 32
+    height = 84
+    width = 84
+    lr = 0.01
+    gamma = 0.98
+    epsilon = 0.95
+    capacity = 256
+    play_times = 128
+    skip_channels = 4

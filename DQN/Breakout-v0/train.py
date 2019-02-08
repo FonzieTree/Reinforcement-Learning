6b1672abc71319068cb0975e5eba7303315a4d5b
Fonzie <675688853@qq.com>
2019-02-08 12:01:14 +0800

    Update train.py

--- DQN/Breakout-v0/train.py
+++ DQN/Breakout-v0/train.py
@@ -1,5 +1,3 @@
-# author: Shuming Fang
-# email: fangshuming519@gmail.com
 import numpy as np
 from PIL import Image
 import tensorflow as tf
@@ -25,6 +23,14 @@ table_s = table_s.astype(np.float32)
 table_s_ = table_s_.astype(np.float32)
 table_r = table_r.astype(np.float32)
 table_a = table_a.astype(np.int32)
+buffer_s = np.random.randn(hp.buffer_size,hp.height,hp.width,hp.skip_channels)
+buffer_s_ = np.random.randn(hp.buffer_size,hp.height,hp.width,hp.skip_channels)
+buffer_r = np.random.randn(hp.buffer_size,)
+buffer_a = np.array([np.random.randint(0,4) for i in range(hp.buffer_size)])
+buffer_s = buffer_s.astype(np.float32)
+buffer_s_ = buffer_s_.astype(np.float32)
+buffer_r = buffer_r.astype(np.float32)
+buffer_a = buffer_a.astype(np.int32)
 
 class Graph:
     def __init__(self, training=True):
@@ -43,13 +49,13 @@ class Graph:
                 self.q_eval = tf.layers.conv2d(self.q_eval, filters=32,kernel_size=4,strides=(2,2),padding='valid',activation=tf.nn.relu)
                 self.q_eval = tf.layers.flatten(self.q_eval)
                 self.q_eval = tf.layers.dense(self.q_eval,256,activation=tf.nn.relu)
-                self.q_eval = tf.layers.dense(self.q_eval,4)
+                self.q_eval = tf.layers.dense(self.q_eval,4,activation=tf.nn.sigmoid)
             with tf.variable_scope('target_net'):
                 self.q_next = tf.layers.conv2d(self.table_s_, filters=16,kernel_size=8,strides=(4,4),padding='valid',activation=tf.nn.relu)
                 self.q_next = tf.layers.conv2d(self.q_next, filters=32,kernel_size=4,strides=(2,2),padding='valid',activation=tf.nn.relu)
                 self.q_next = tf.layers.flatten(self.q_next)
                 self.q_next = tf.layers.dense(self.q_next,256,activation=tf.nn.relu)
-                self.q_next = tf.layers.dense(self.q_next,4)
+                self.q_next = tf.layers.dense(self.q_next,4,activation=tf.nn.sigmoid)
             with tf.variable_scope('q_target'):
                 q_target = self.table_r + hp.gamma * tf.reduce_max(self.q_next, axis=1, name='Qmax_s_')
                 self.q_target = tf.stop_gradient(q_target)
@@ -62,7 +68,6 @@ class Graph:
                 self._train_op = tf.train.RMSPropOptimizer(hp.lr).minimize(self.loss)
             t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target_net')
             e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='eval_net')
-
             with tf.variable_scope('hard_replacement'):
                 self.target_replace_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
             
@@ -113,21 +118,21 @@ if __name__ == '__main__':
                     observation_video[:,:,0:3] = observation_video[:,:,1:4]
                     observation_video[:,:,3] = observation_
                     counter+=1
-                    if counter%hp.capacity==0:
+                buffer_s[0:hp.buffer_size-hp.capacity,:,:,:] = buffer_s[hp.capacity:hp.buffer_size,:,:,:]
+                buffer_s_[0:hp.buffer_size-hp.capacity,:,:,:] = buffer_s_[hp.capacity:hp.buffer_size,:,:,:]
+                buffer_r[0:hp.buffer_size-hp.capacity] = buffer_r[hp.capacity:hp.buffer_size]
+                buffer_a[0:hp.buffer_size-hp.capacity] = buffer_a[hp.capacity:hp.buffer_size]
+                buffer_s[-hp.capacity:] = table_s
+                buffer_s_[-hp.capacity:] = table_s_
+                buffer_r[-hp.capacity:] = table_r
+                buffer_a[-hp.capacity:] = table_a
+                if counter>hp.buffer_size:
+                    for j in range(8):
                         idx = np.random.randint(0, hp.capacity, hp.batch_size)
                         table_sample_s = table_s[idx]
                         table_sample_s_ = table_s_[idx]
                         table_sample_r = table_r[idx]
                         table_sample_a = table_a[idx]
-                        if np.sum(table_r[table_r==10.0])>0:
-                            exp_idx = np.where(table_r==1.0)[0]
-                            table_sample_s[0:len(exp_idx)] = table_s[exp_idx]
-                            table_sample_s_[0:len(exp_idx)] = table_s_[exp_idx]
-                            table_sample_r[0:len(exp_idx)] = table_r[exp_idx]
-                            table_sample_a[0:len(exp_idx)] = table_a[exp_idx]
-                        for j in range(4):
-                            _,loss = sess.run([g._train_op,g.loss],feed_dict={g.table_s:table_sample_s,g.table_s_:table_sample_s_,g.table_r:table_sample_r,g.table_a:table_sample_a})
-                            print('Epsilon: ',i, 'Step: ',j, 'Loss: ', loss)
-                        _ = sess.run(g.target_replace_op,feed_dict={g.table_s:table_sample_s,g.table_s_:table_sample_s_,g.table_r:table_sample_r,g.table_a:table_sample_a})
-
-
+                        _,loss = sess.run([g._train_op,g.loss],feed_dict={g.table_s:table_sample_s,g.table_s_:table_sample_s_,g.table_r:table_sample_r,g.table_a:table_sample_a})
+                        print('Epsilon: ',i, 'Step: ',j, 'Loss: ', loss)
+                    _ = sess.run(g.target_replace_op,feed_dict={g.table_s:table_sample_s,g.table_s_:table_sample_s_,g.table_r:table_sample_r,g.table_a:table_sample_a})

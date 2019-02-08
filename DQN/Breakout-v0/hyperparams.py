34812551f5855a99f0a68359423f88c56e12678d
Fonzie <675688853@qq.com>
2019-02-08 12:01:54 +0800

    Update hyperparams.py

--- DQN/Breakout-v0/hyperparams.py
+++ DQN/Breakout-v0/hyperparams.py
@@ -1,10 +1,10 @@
 class Hyperparams:
-    batch_size = 32
+    batch_size = 64
     height = 84
     width = 84
     lr = 0.01
     gamma = 0.98
     epsilon = 0.95
     capacity = 256
-    play_times = 128
     skip_channels = 4
+    buffer_size = 4000
